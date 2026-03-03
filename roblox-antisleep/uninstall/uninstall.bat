cfc96e1839e5d7d55692d62ed0784d436bf6a84c
hedgehodg01 <hedgehodg01@gmail.com>
2026-03-03 19:49:32 +0300

    Add files via upload

--- roblox-antisleep/uninstall/uninstall.bat
+++ roblox-antisleep/uninstall/uninstall.bat
@@ -1,12 +1,12 @@
 @echo off
 echo Удаление программы...
-taskkill /f /im main.exe >nul 2>&1
+taskkill /f /im main.py >nul 2>&1
 
 REM Удаление из автозагрузки
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsUpdateSvc" /f >nul 2>&1
 
 REM Удаление файлов программы
-del "%~dp0main.exe" >nul 2>&1
+del "%~dp0main.py" >nul 2>&1
 
 REM Удаление папки программы
 rmdir "%~dp0" >nul 2>&1
